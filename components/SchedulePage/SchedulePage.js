4033b0184986605d0db9d57df7bcd40838f2269b
g-crow <genevieve@fiveable.me>
2022-05-13 18:24:38 -0600

    update schedule

--- components/SchedulePage/SchedulePage.js
+++ components/SchedulePage/SchedulePage.js
@@ -79,28 +79,6 @@ const SchedulePage = () => {
         url="https://freestoneclimbing.com/calendar/"
         time="5-6PM"
       />
-      <Event
-        day="Wednesday"
-        title="Vinyasa~Fusion"
-        location="Purusa Yoga"
-        description="This class blends vinyasa flow with breathwork and techniques designed to tap into the benefits of the parasympathetic nervous system, the system involved in feeling safe in our minds and bodies. Participants will be guided through a balanced physical practice as well as given the opportunity to practice more subtle attentional skills."
-        price="$25 drop-in. Class packages and memberships available."
-        url="https://purusayoga.org/schedule/"
-        time="6-7PM"
-      />
-      <Event
-        day="Thursday"
-        title="Vinyasa Flow"
-        location="Freestone Climbing Center"
-        description="This Ashtanga-inspired flow focuses on full-body mobility with special
-        attention paid to the spine. We'll stretch and strengthen major muscle groups, with the understanding that strength is not rigid; strength
-        is knowing your center and wrapping yourself around it. The sequence
-        incorporates traditional yoga asanas along with drills specifically geared
-        toward climbers (hello, shoulders and hip flexors!)."
-        price="$19 drop-in. Memberships available."
-        url="https://freestoneclimbing.com/calendar/"
-        time="5-6PM"
-      />
     </div>
   );
 };
