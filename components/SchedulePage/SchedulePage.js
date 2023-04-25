a711bbcfa294648ccee6e0e7709ccf4c2b6318d8
g-crow <genevieve@fiveable.me>
2023-04-24 20:37:02 -0600

    Update schedule - does this deploy process still work?

--- components/SchedulePage/SchedulePage.js
+++ components/SchedulePage/SchedulePage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from "./SchedulePage.module.css";
-import { Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
 
 const Event = ({
   day,
@@ -66,8 +66,25 @@ const SchedulePage = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Classes</h1>
+      <Typography
+        variant="h6"
+        className={styles.subtitle}
+        style={{ paddingBottom: "10px" }}
+      >
+        I'm taking a break from a regular teaching schedule these days. You
+        can still find me subbing in a few Missoula studios, dates listed below:
+      </Typography>
       <Event
-        day="Tuesday"
+        day="Thursday, May 4th"
+        title="Yoga"
+        location="Inner Harmony Yoga"
+        description="An all-around, balanced flow suitable for all levels. We'll move through a series of postures designed to loosen, strengthen, and balance the body; just the right intensity for a mid-day, mid-week pick-me-up."
+        price="$14 drop-in. Memberships available."
+        url="http://yogainmissoula.com/Schedule/Schedule.html"
+        time="Noon-1:00PM"
+      />
+      <Event
+        day="Wednesday, May 10th"
         title="Vinyasa Flow"
         location="Freestone Climbing Center"
         description="This Ashtanga-inspired flow focuses on full-body mobility with special
@@ -77,8 +94,17 @@ const SchedulePage = () => {
         toward climbers (hello, shoulders and hip flexors!)."
         price="$19 drop-in. Memberships available."
         url="https://freestoneclimbing.com/calendar/"
-        time="5-6PM"
+        time="7-8:30PM"
       />
+      <Typography
+        variant="h6"
+        className={styles.subtitle}
+        style={{ paddingTop: "20px" }}
+      >
+        Offerings in the works I'm excited to unveil soon: SUP yoga experiences
+        on Flathead Lake, August 2023; a three-week inversion intensive at several
+        Missoula studios, sometime this fall - stay tuned for details!
+      </Typography>
     </div>
   );
 };
